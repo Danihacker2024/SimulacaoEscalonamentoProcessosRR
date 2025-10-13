94c9b9e1169842589c626cf28bde8f329e939d7e
Daniel <danielnevs2020@gmail.com>
2025-10-13 17:11:52 -0300

    FIX: Fila de bloqueados

--- Simulacao.c
+++ Simulacao.c
@@ -52,7 +52,7 @@ void ExibirProcesso(Processo proc,int x, int y){
 	printf("Tempo: (%d/%d)",proc.tempo_exec,proc.CPU_Burst);
 }
 
-void ExibirProcessoSemTempoRecursos(Processo proc,int x, int y){
+void ExibirProcessoBloqueado(Processo proc,int x, int y){
 	gotoxy(x,y);
 	printf("Pid: %d",proc.pid);
 	y++;
@@ -61,6 +61,12 @@ void ExibirProcessoSemTempoRecursos(Processo proc,int x, int y){
 	y++;
 	gotoxy(x,y);
 	printf("Prioridade: %d",proc.prioridade);
+	y++;
+	gotoxy(x,y);
+	printf("Filhos: %d",proc.filhos);
+	y++;
+	gotoxy(x,y);
+	printf("Tempo: (%d/%d)",proc.timeblock,proc.total);
 }
 
 
@@ -93,7 +99,7 @@ void Simulacao(){
 	gerarProcessos(&descProntos);
 	//Exibir(descritor);
 	CPU->PCB = dequeue(&descProntos);
-    while(CPU != NULL || !QisEmpty(descProntos.qtde)){
+    while((CPU != NULL) || !QisEmpty(descProntos.qtde) || !flag.HD || !flag.mouse || !flag.teclado){
 	    if(kbhit()){
 	    	opcao = getch();
 	    	if(opcao==27){
@@ -111,61 +117,100 @@ void Simulacao(){
 			if(CPU!=NULL && CPU->PCB.tempo_exec<CPU->PCB.CPU_Burst){
 				
 				ExibirProcesso(CPU->PCB,190,4);
-				if(CPU->PCB.tempo_exec==1){
-					sorteioNecRec = rand() % 3;
-					if(sorteioNecRec>0)
-						sorteioRec=(rand() % 3) + 1;
-					else
-						sorteioRec=-1;
-				}
+				sorteioNecRec = rand() % 10;
+				if(sorteioNecRec==1)
+					sorteioRec=(rand() % 3) + 1;
+				else
+					sorteioRec=-1;
 				
 				if(sorteioRec==1){
 					if(flag.HD){
-						CPU->PCB.Recursos.HD=1;
-						CPU->PCB.Recursos.mouse=0;
-						CPU->PCB.Recursos.teclado=0;
-						flag.HD=0;
 						HD=CPU->PCB;
+						HD.Recursos.HD=1;
+						HD.Recursos.mouse=0;
+						HD.Recursos.teclado=0;
+						flag.HD=0;
+						HD.timeblock=0;
+						HD.total = (rand()%30) + 1;
+						if(!QisEmpty(descProntos.qtde)){
+							CPU->PCB = dequeue(&descProntos);
+							quantum=0;
+						} else{
+							CPU=NULL;
+						}
 					}else if(!flag.HD && !CPU->PCB.Recursos.HD){
 						enqueue(&descHD,CPU->PCB);
 						if(!QisEmpty(descProntos.qtde)){
 							CPU->PCB = dequeue(&descProntos);
 							quantum=0;
+						}else{
+							CPU=NULL;
+							gotoxy(190,4);
+							printf("SEM PROCESSO NA CPU");
 						}
+						
 					}
 					
 				}else if(sorteioRec==2){
 					if(flag.teclado){
-						CPU->PCB.Recursos.teclado=1;
-						CPU->PCB.Recursos.HD=0;
-						CPU->PCB.Recursos.mouse=0;
-						flag.teclado=0;
 						Teclado=CPU->PCB;
+						Teclado.Recursos.mouse=1;
+						Teclado.Recursos.HD=0;
+						Teclado.Recursos.teclado=0;
+						flag.teclado=0;
+						Teclado.timeblock=0;
+						Teclado.total = (rand()%30) + 1;
+						if(!QisEmpty(descProntos.qtde)){
+							CPU->PCB = dequeue(&descProntos);
+							quantum=0;
+						}else{
+							CPU=NULL;
+							gotoxy(190,4);
+							printf("SEM PROCESSO NA CPU");
+						}
 					}else if(!flag.teclado && !CPU->PCB.Recursos.teclado){
 						enqueue(&descTeclado,CPU->PCB);
 						if(!QisEmpty(descProntos.qtde)){
 							CPU->PCB = dequeue(&descProntos);
 							quantum=0;
+						}else{
+							CPU=NULL;
+							gotoxy(190,4);
+							printf("SEM PROCESSO NA CPU");
 						}
 					}
 					
 				}else if(sorteioRec==3){
 					if(flag.mouse){
-							CPU->PCB.Recursos.mouse=1;
-							CPU->PCB.Recursos.HD=0;
-							CPU->PCB.Recursos.teclado=0;
-							flag.mouse=0;
-							Mouse=CPU->PCB;
-						}else if(!flag.mouse && !CPU->PCB.Recursos.mouse){
-							enqueue(&descMouse,CPU->PCB);
-							if(!QisEmpty(descProntos.qtde)){
-								CPU->PCB = dequeue(&descProntos);
-								quantum=0;
-							}
+						Mouse=CPU->PCB;
+						Mouse.Recursos.mouse=1;
+						Mouse.Recursos.HD=0;
+						Mouse.Recursos.teclado=0;
+						Mouse.timeblock=0;
+						flag.mouse=0;
+						Mouse.total = (rand()%30) + 1;
+						if(!QisEmpty(descProntos.qtde)){
+							CPU->PCB = dequeue(&descProntos);
+							quantum=0;
+							}else{
+							CPU=NULL;
+							gotoxy(190,4);
+							printf("SEM PROCESSO NA CPU");
+						}
+					}else if(!flag.mouse && !CPU->PCB.Recursos.mouse){
+						enqueue(&descMouse,CPU->PCB);
+						if(!QisEmpty(descProntos.qtde)){
+							CPU->PCB = dequeue(&descProntos);
+							quantum=0;
+						}else{
+							CPU=NULL;
+							gotoxy(190,4);
+							printf("SEM PROCESSO NA CPU");
+						}
 					}	
 				}
 				if(flagExec){
-					sorteioFork = rand() % 3;
+					sorteioFork = rand() % 10;
 					if(sorteioFork==1){
 						CPU->PCB.filhos++;
 						Fork(CPU->PCB, &descProntos, CPU->PCB.filhos);
@@ -184,24 +229,6 @@ void Simulacao(){
 			}else{
 				gotoxy(190,4);
 				printf("FINALIZADO");
-				if(CPU->PCB.Recursos.HD){
-					CPU->PCB.Recursos.HD=0;
-					flag.HD=1;
-					while(!QisEmpty(descHD.qtde))
-						enqueue(&descProntos,dequeue(&descHD));
-				}
-				if(CPU->PCB.Recursos.teclado){
-					CPU->PCB.Recursos.teclado=0;
-					flag.teclado=1;
-					while(!QisEmpty(descTeclado.qtde))
-						enqueue(&descProntos,dequeue(&descTeclado));
-				}
-				if(CPU->PCB.Recursos.mouse){
-					CPU->PCB.Recursos.mouse=0;
-					flag.mouse=1;
-					while(!QisEmpty(descMouse.qtde))
-						enqueue(&descProntos,dequeue(&descMouse));
-				}
 				if(CPU->PCB.filhos>0)
 					enqueue(&descWait, CPU->PCB);	
 				if(CPU->PCB.FlagFork){
@@ -242,7 +269,7 @@ void Simulacao(){
 				x=6;
 				aux = descHD.inicio;
 				while(aux!=NULL){
-					ExibirProcesso(aux->PCB,x,13);
+					ExibirProcessoBloqueado(aux->PCB,x,13);
 					x+=15;
 					aux=aux->prox;
 				}
@@ -251,7 +278,7 @@ void Simulacao(){
 				x=6;
 				aux = descTeclado.inicio;
 				while(aux!=NULL){
-					ExibirProcesso(aux->PCB,x,22);
+					ExibirProcessoBloqueado(aux->PCB,x,22);
 					x+=15;
 					aux=aux->prox;
 				}
@@ -260,7 +287,7 @@ void Simulacao(){
 			    x=6;
 				aux = descMouse.inicio;
 				while(aux!=NULL){
-					ExibirProcesso(aux->PCB,x,31);
+					ExibirProcessoBloqueado(aux->PCB,x,31);
 					x+=15;
 					aux=aux->prox;
 				}
@@ -277,18 +304,45 @@ void Simulacao(){
 			if(flag.HD){
 				gotoxy(190,15);
 				printf("DISPONIVEL");
-			}else
-				ExibirProcessoSemTempoRecursos(HD,190,13);
+			}else{
+				HD.timeblock++;
+				ExibirProcessoBloqueado(HD,190,13);
+				if(HD.timeblock>=HD.total){
+					enqueue(&descProntos,HD);
+					if(!QisEmpty(descHD.qtde))
+						HD = dequeue(&descHD);
+					else
+						flag.HD=1;
+				}
+			}
 			if(flag.teclado){
 				gotoxy(190,24);
 				printf("DISPONIVEL");
-			}else
-				ExibirProcessoSemTempoRecursos(Teclado,190,22);
+			}else{
+				Teclado.timeblock++;
+				ExibirProcessoBloqueado(Teclado,190,22);
+				if(Teclado.timeblock>=Teclado.total){
+					enqueue(&descProntos,Teclado);
+					if(!QisEmpty(descTeclado.qtde))
+						Teclado = dequeue(&descTeclado);
+					else
+						flag.teclado=1;
+				}
+			}
 			if(flag.mouse){
 				gotoxy(190,33);
 				printf("DISPONIVEL");
-			}else
-				ExibirProcessoSemTempoRecursos(Mouse,190,31);
+			}else{
+				Mouse.timeblock++;
+				ExibirProcessoBloqueado(Mouse,190,31);
+				if(Mouse.timeblock>=Mouse.total){
+					enqueue(&descProntos,Teclado);
+					if(!QisEmpty(descMouse.qtde))
+						Mouse = dequeue(&descMouse);
+					else
+						flag.mouse=1;
+				}
+			}
 			//sorteio de inclusao de novos processos
 			if(flagExec){
 				sorteioInclusao=rand() % 10;
